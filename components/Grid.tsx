7e384e8c9a62a08521f9c5c63f617d991684eede
Mikkode <mic.warin@gmail.com>
2024-06-26 00:21:59 +0200

    50%

---
+++ components/Grid.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const items = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible with Time Zones to Accommodate Global Clients",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionate Technologist Driven by Innovation and Development",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Currently diving into the world of AI technology, honing my skills.",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Interested in Collaborating on Your Next Project ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export default function Grid() {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {items.map((item) => (
+          <BentoGridItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+}
