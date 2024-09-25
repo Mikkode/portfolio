88a9f00b0c665f84cd4b8f30b98e844852a8e620
Mikkode <mic.warin@gmail.com>
2024-09-25 22:21:47 +0200

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-954C7MEDBX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-954C7MEDBX');
+          `}
+        </Script>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -27,6 +42,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://carrotpixel.com", // Remplace par ton URL
-  generateRobotsTxt: true, // Génère aussi le fichier robots.txt
-  sitemapSize: 5000, // Nombre maximum de pages par fichier de sitemap
-  exclude: [], // Exclure certaines pages si nécessaire
-};
